75cda28687f9a878df95eea5d4512292852ea128
anand1220 <ak3332437@gmail.com>
2023-12-01 22:12:39 +0530

    navbar updated

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 const form = document.querySelector('form');
 const resetButton = document.querySelector('.reset');
 let root = document.querySelector('.result');
+let icon = document.querySelector('.icon');
+let links = document.getElementsByTagName('a');
+
+icon.addEventListener('click', function () {
+    for (let i = 1; i < links.length; i++) {
+        if (links[i].style.display === "block") {
+            links[i].style.display = "none";
+        } else {
+            links[i].style.display = "block";
+        }
+    }
+})
+
+
+
+
+// form submit
 form.addEventListener('submit', function (e) {
     e.preventDefault();
 
